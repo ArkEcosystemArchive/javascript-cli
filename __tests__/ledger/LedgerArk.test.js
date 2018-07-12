6742eef1a0bc815a1d585aae214aab03addd9919
wownmedia <mschot@wownmedia.com>
2018-07-12 18:14:02 +0000

    fix: correct Jest tests comments

--- __tests__/ledger/LedgerArk.test.js
+++ __tests__/ledger/LedgerArk.test.js
@@ -3,31 +3,31 @@
 const LedgerArk = require('../../lib/ledger/LedgerArk.js')
 
 describe('LedgerArk', () => {
-  it('should be defined', () => {
+  it('should be an object', () => {
     expect(LedgerArk).toBeObject()
   })
 })
 
 describe('LedgerArk.setComm', () => {
-  it('should be defined', () => {
+  it('should be a function', () => {
     expect(LedgerArk.setComm).toBeFunction()
   })
 })
 
 describe('LedgerArk.getPublicKey', () => {
-  it('should be defined', () => {
+  it('should be a function', () => {
     expect(LedgerArk.getPublicKey).toBeFunction()
   })
 })
 
 describe('LedgerArk.signTransaction', () => {
-  it('should be defined', () => {
+  it('should be a function', () => {
     expect(LedgerArk.signTransaction).toBeFunction()
   })
 })
 
 describe('LedgerArk.getAppConfiguration', () => {
-  it('should be defined', () => {
+  it('should be a function', () => {
     expect(LedgerArk.getAppConfiguration).toBeFunction()
   })
 })
