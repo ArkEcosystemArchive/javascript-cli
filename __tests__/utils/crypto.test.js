1600f80ed6f256a95466994f112b318fed3c08a8
wownmedia <mschot@wownmedia.com>
2018-08-27 08:17:14 +0000

    v2: update second signature command to use v2 libraries

--- __tests__/utils/crypto.test.js
+++ __tests__/utils/crypto.test.js
@@ -120,6 +120,22 @@ describe('crypto.createVoteTransaction', () => {
   })
 })
 
+describe('crypto.createignatureTransaction', () => {
+  it('should be a function', () => {
+    expect(crypto.createignatureTransaction).toBeFunction()
+  })
+
+  it('should create a valid transaction', () => {
+    const passphrase = 'this is a top secret passphrase'
+    const secondSecret = 'test secret'
+    const fee = 1000
+    const transaction = crypto.createignatureTransaction(passphrase, secondSecret, fee)
+
+    expect(transaction).toContainKeys(['fee', 'id', 'senderPublicKey', 'signature', 'type', 'asset'])
+    expect(transaction.asset.signature).toContainKey('publicKey')
+  })
+})
+
 describe('crypto.getTransactionId', () => {
   it('should be a function', () => {
     expect(crypto.getTransactionId).toBeFunction()
